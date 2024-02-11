bc6f1e6af3ecc00731ebcc754037f4438c9a369d
iamjhonattan <iamjhonattan@gmail.com>
2024-02-11 15:28:10 -0400

    commit-11-02-24-03:28pm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,17 @@
+// Home.jsx
+
 import React from 'react';
 import Header from '../components/home/Header';
 import Body from '../components/home/Body';
 import Footer from '../components/home/Footer';
-import { Helmet } from 'react-helmet';
 import '../styles/home/Home.css';
 
 const Home = () => {
   return (
     <div className="home">
-      <Helmet>
-        <title>Iglesia El Shaddai - </title>
-        <meta name="description" content="Descripción de tu página de inicio" />
-        {/* Agrega otras meta etiquetas si es necesario */}
-      </Helmet>
       <Header
         title="El Shaddai - Iglesia Evangélica Misionera Pentecostés"
-        description="Iglesia Evangélica Misionera Pentecostés"
+        description="Iglesia Evangélica Misionera Pentecostés, La Candelaria, Parroquia Caña de Azúcar"
         keywords="iglesia, evangelica, misionera, el shaddai"
       />
       <Body />
