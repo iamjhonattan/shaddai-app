34ff331417b93495d6f6e6edc442bb5b5da3363c
iamjhonattan <iamjhonattan@gmail.com>
2024-02-11 07:38:34 -0400

    commit-11-02-24-07:38am

--- src/data/videoData.jsx
+++ src/data/videoData.jsx
@@ -2,7 +2,7 @@
 import imgPlayer from '../image/img-player.png';
 
 const videoData = {
-  live: false,
+  live: true,
   url: 'https://www.facebook.com/shaddai.live/videos/1103600610770299/',
   imagePath: imgPlayer,
   nextStreamTime: '2024-02-12 09:00:00',
